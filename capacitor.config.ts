177e17eba92d5776fe5e29888c60bc9b09959374
AlexAraujoDeBraga <alexbraga107@gmail.com>
2024-02-10 19:19:49 -0300

    inserindo versao appflow em development

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery-final',
   webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: 'b76dc647',
+      channel: 'development',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  },
   server: {
     androidScheme: 'https'
   }
